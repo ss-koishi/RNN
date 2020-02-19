ad274e1dedbb5f6418c356a7459809691f3df9f1
ss-koishi <yuuki100201@gmail.com>
2020-02-19 05:02:09 +0000

    optimized

--- make_data.py
+++ make_data.py
@@ -17,3 +17,10 @@ def make_noised_sin():
     df["sin_t"] = df.t.apply(lambda x: math.sin(x * (2 * math.pi / steps_per_cycle)+ random.uniform(-1.0, +1.0) * random_factor))
 
     return df[["sin_t"]]
+
+def make_sincos():
+    df = pd.DataFrame(np.arange(0, 4000), columns=['t'])
+    df["sincos"] = df.t.apply(lambda x: math.sin(np.pi * x / 20) + math.cos(np.pi * x * 3 / 20))
+    df["sincos"] = df.sincos.apply(lambda x: x * (0.75 + 0.5 * random.random()))
+
+    return df[["sincos"]]
--- rnn.py
+++ rnn.py
@@ -5,7 +5,7 @@ from tensorflow.python.keras.callbacks import EarlyStopping
 
 import matplotlib.pyplot as plt
 
-from make_noised_sin import make_noised_sin
+import make_data as md
 
 import pandas as pd
 import numpy as np
@@ -13,7 +13,7 @@ import math
 import random
 
 io = 1
-hidden = 300
+hidden = 50
 
 def load_data(data, n_prev):
 
@@ -39,8 +39,8 @@ def train_test_split(df, test_size=0.1, n_prev=100):
 
 def main():
 
-    sin = make_noised_sin()
-    size = 100
+    sin = md.make_noised_sin()
+    size = 60
     (x_train, y_train), (x_test, y_test) = train_test_split(sin, n_prev = size)
 
     model = Sequential([
@@ -51,27 +51,23 @@ def main():
 
     model.compile(loss='mean_squared_error', optimizer='adam')
     stopping = EarlyStopping(monitor='val_loss', mode='auto', patience=10)
-    model.fit(x_train, y_train, batch_size=256, nb_epoch=100, validation_split=0.1, callbacks=[stopping])
+    model.fit(x_train, y_train, batch_size=256, nb_epoch=1000, validation_split=0.1, callbacks=[stopping])
 
-    pred_out = model.predict(x_test)
-    predict = pred_out
-    output = []
-    future_data = x_test
-    future_steps = 400
+    result = []
+    future_steps = 1000
+    future_data = [x_train[-1:][-1:][-size:]]
     for i in range(future_steps):
       print(i)
-      future_data = np.delete(future_data, 0, 0)
-      future_data = np.append(future_data, predict[-size:]).reshape(300, 100, 1)
-
-      predict = model.predict(future_data)
-      output = np.append(output, predict[len(predict) - 1])
-
+      pred = model.predict(future_data)
+      future_data = np.delete(future_data, 0)
+      future_data = np.append(future_data, pred[-1:]).reshape(1, size, 1)
+      result = np.append(result, pred[-1:])
 
     plt.figure()
     #plt.plot(y_test.flatten())
-    plt.plot(range(0, 300), y_test.flatten(), label='input')
-    plt.plot(range(0, 300), pred_out.flatten(), label='predict')
-    plt.plot(range(300, 300 + future_steps), output.flatten(), label='future')
+    output = y_train[-100:]
+    plt.plot(range(0, len(output)), output, label='input')
+    plt.plot(range(len(output), len(output) + future_steps), result, label='future')
     plt.title('Sin Curve prediction')
     plt.legend(loc='upper right')
     plt.savefig('result.png')
