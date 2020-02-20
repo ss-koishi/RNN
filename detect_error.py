dd64cbe03dca4cf32c21fed4d51abe16226a6c06
ss-koishi <yuuki100201@gmail.com>
2020-02-20 15:03:18 +0900

    edited

--- detect_error.py
+++ detect_error.py
@@ -4,7 +4,7 @@ from tensorflow.python.keras.layers.recurrent import LSTM
 from tensorflow.python.keras.callbacks import EarlyStopping
 from tensorflow.python.keras.models import load_model
 
-from sklearn import preprocessing
+from sklearn.preprocessing import StandardScaler
 
 import matplotlib.pyplot as plt
 
@@ -26,26 +26,29 @@ def make_dataset(df):
 
     return np.array(ret)
 
+
 def main():
 
     model = load_model('./model/model.h5')
     model.compile(loss='mean_squared_error', optimizer='adam')
 
+    scaler = StandardScaler()
     db = pd.read_csv('./wave.csv')
+    db['ng'] = scaler.fit_transform(db['ng'])
 
-    data = make_dataset(db[['ng']].iloc[0:-1])
-    pred = pd.DataFrame(model.predict(data))
-    print(pred)
+    data = make_dataset(db['ng'])
+    pred = model.predict(data)
+
     mse = np.sqrt(np.sum((pred - data)**2, axis=1))
-    print(mse)    
+    print(mse)
     plt.figure()
     #plt.plot(y_test.flatten())
     plt.plot(range(0, len(db['ng'])), db['ng'], label='input')
-    plt.plot(range(sequence_size, sequence_size + len(pred)), pred, label='predict') 
+    plt.plot(range(sequence_size, sequence_size + len(pred)), pred, label='predict')
     plt.plot(range(sequence_size, sequence_size + len(mse)), mse, label='mse')
     plt.title('Wave prediction')
     plt.legend(loc='upper right')
-    plt.savefig('result.png')
+    plt.savefig('error.png')
 
 
 if __name__ == '__main__':
--- prediction.py
+++ prediction.py
@@ -13,31 +13,37 @@ import math
 import random
 
 io = 1
-hidden = 300
-sequence_size = 100
+hidden = 50
+sequence_size = 80
+
 
 def load_data(data):
 
     x, y = [], []
     db = pd.DataFrame(data)
     for i in range(len(data) - sequence_size):
-        x.append(db.iloc[i: i + sequence_size].value())
-        y.append(db.iloc[i + sequence_size].value())
+        x.append(db.iloc[i: i + sequence_size].as_matrix())
+        y.append(db.iloc[i + sequence_size].as_matrix())
 
     return np.array(x), np.array(y)
 
 
 def make_dataset(test_size=0.1):
-  
-    scaler = MinMaxScaler()
+
+    scaler = StandardScaler()
     db = pd.read_csv('./wave.csv')
     db['ok'] = scaler.fit_transform(db['ok'])
 
-    pos = int(len(db) - sequence_size)
+    pos = int(len(db) * (1 - test_size))
+    x_train, y_train = load_data(db['ok'].iloc[0:pos])
+    x_test, y_test = load_data(db['ok'].iloc[pos:])
+
+    return (x_train, y_train), (x_test, y_test)
+
 
 def main():
 
-    x_train, y_train = make_dataset(wave)
+    (x_train, y_train), (x_test, y_test) = make_dataset()
 
     model = Sequential([
         LSTM(hidden, batch_input_shape=(None, sequence_size, io), return_sequences=False),
@@ -47,28 +53,30 @@ def main():
 
     model.compile(loss='mean_squared_error', optimizer='adam')
     stopping = EarlyStopping(monitor='val_loss', mode='auto', patience=10)
-    model.fit(x_train, y_train, batch_size=256, nb_epoch=100, validation_split=0.1, callbacks=[stopping])
+    model.fit(x_train, y_train, batch_size=256, epochs=100, validation_split=0.2, callbacks=[stopping])
 
     result = []
     future_steps = 400
-    future_data = x_train[-sequence_size:]
+    future_data = x_test[-1:][-sequence_size:]
     for i in range(future_steps):
-      print(i)
-      pred = model.predict(future_data)
-      future_data = np.delete(future_data, 0)
-      future_data = np.append(future_data, pred[-1:])
+        print(i)
+        pred = model.predict(future_data)
+        future_data = np.delete(future_data, 0)
+        future_data = np.append(future_data, pred[-1:])
 
-      result = np.append(result, pred[-1:])
+        result = np.append(result, pred[-1:])
 
-
+    input_pred = model.predict(x_test).flatten()
     plt.figure()
-    #plt.plot(y_test.flatten())
-    plt.plot(range(0, len(x_train)), x_train, label='input')
-    plt.plot(range(len(x_train), len(x_train) + future_steps), result, label='future')
+    plt.plot(range(0, len(y_train)), y_train, label='train')
+    plt.plot(range(len(y_train), len(y_train) + len(y_test)), y_test, label='input')
+    plt.plot(range(len(y_train), len(y_train) + len(input_pred)), input_pred, label='input')
+    plt.plot(range(len(y_train) + len(input_pred), len(y_train) + len(input_pred) + future_steps), result, label='future')
     plt.title('Wave prediction')
     plt.legend(loc='upper right')
     plt.savefig('result.png')
 
+    model.save('./model/model.h5')
 
 if __name__ == '__main__':
     main()
