219cf3c8ad020a82a5efcfb9f4686ae47535ed6d
ss-koishi <yuuki100201@gmail.com>
2020-02-21 07:32:51 +0000

    period

--- detect_error.py
+++ detect_error.py
@@ -4,25 +4,22 @@ from tensorflow.python.keras.layers.recurrent import LSTM
 from tensorflow.python.keras.callbacks import EarlyStopping
 from tensorflow.python.keras.models import load_model
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import minmax_scale
 
 import matplotlib.pyplot as plt
 
 import pandas as pd
 import numpy as np
 import math
-import random
+import random 
 
-io = 1
-hidden = 25
-sequence_size = 80
-
+sequence_size = 20
 
 def make_dataset(df):
 
     ret = []
     for i in range(len(df) - sequence_size):
-        ret.append(df.iloc[i:i + sequence_size].as_matrix())
+        ret.append(df.iloc[i:i + sequence_size].as_matrix().flatten())
 
     return np.array(ret)
 
@@ -30,22 +27,44 @@ def make_dataset(df):
 def main():
 
     model = load_model('./model/model.h5')
-    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.compile(loss='mse', optimizer='adam')
 
-    scaler = StandardScaler()
+    #scaler = StandardScaler()
     db = pd.read_csv('./wave.csv')
-    db['ng'] = scaler.fit_transform(db['ng'])
+    mean, var = None, None
+    #with open('parameters.txt', 'r') as f:
+    #  mean = float(f.readline())
+    #  var = float(f.readline())
 
-    data = make_dataset(db['ng'])
-    pred = model.predict(data)
+    #scaler.fit(db[['ok']]);
+    #scaler.mean_ = np.array(mean)
+    #scaler.var_ = np.array(var)
+    db['ok'] = minmax_scale(db[['ok']])
+    db['ng'] = minmax_scale(db[['ng']])
+    db['ok2'] = minmax_scale(db[['ok2']])
 
-    mse = np.sqrt(np.sum((pred - data)**2, axis=1))
-    print(mse)
+    ok_data = make_dataset(db[['ok']].iloc[0:-1])
+    ng_data = make_dataset(db[['ng']].iloc[0:-1])
+   
+    ok2_data = make_dataset(db[['ok2']].iloc[0:-1])
+    
+    ok_pred = model.predict(ok_data)
+    ng_pred = model.predict(ng_data)
+    ok2_pred = model.predict(ok2_data)
+    ok_mse = np.sqrt(np.sum((ok_pred - ok_data)**2, axis=1))
+    ng_mse = np.sqrt(np.sum((ng_pred - ng_data)**2, axis=1))
+    ok2_mse = np.sqrt(np.sum((ok2_pred - ok2_data)**2, axis=1))
+
     plt.figure()
-    #plt.plot(y_test.flatten())
-    plt.plot(range(0, len(db['ng'])), db['ng'], label='input')
-    plt.plot(range(sequence_size, sequence_size + len(pred)), pred, label='predict')
-    plt.plot(range(sequence_size, sequence_size + len(mse)), mse, label='mse')
+    ok_pred = ok_pred[:, -1:]
+    ng_pred = ng_pred[:, -1:]
+    #plt.plot(range(0, len(db['ng'])), db['ng'], label='ng_input')
+    #plt.plot(range(0, len(db['ok'])), db['ok'], label='ok_input')
+    #plt.plot(range(sequence_size, sequence_size + len(ng_pred)), ng_pred, label='NG_predict')
+    #plt.plot(range(sequence_size, sequence_size + len(ok_pred)), ok_pred, label='OK_predict')
+    plt.plot(range(sequence_size, sequence_size + len(ng_mse)), ng_mse, label='NG mse')
+    plt.plot(range(sequence_size, sequence_size + len(ok_mse)), ok_mse, label='OK mse')
+    plt.plot(range(sequence_size, sequence_size + len(ok2_mse)), ok2_mse, label='OK2 mse')
     plt.title('Wave prediction')
     plt.legend(loc='upper right')
     plt.savefig('error.png')
--- prediction.py
+++ prediction.py
@@ -1,9 +1,10 @@
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers.core import Dense, Activation
+from tensorflow.python.keras.layers import Dense, Activation, Input
 from tensorflow.python.keras.layers.recurrent import LSTM
 from tensorflow.python.keras.callbacks import EarlyStopping
+from tensorflow.python.keras.regularizers import l1, l2
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import minmax_scale
 
 import matplotlib.pyplot as plt
 
@@ -12,66 +13,98 @@ import numpy as np
 import math
 import random
 
+import sys
+
 io = 1
-hidden = 50
-sequence_size = 80
+hidden = 150
+sequence_size = 20
 
 
 def load_data(data):
 
-    x, y = [], []
+    x = []
     db = pd.DataFrame(data)
     for i in range(len(data) - sequence_size):
-        x.append(db.iloc[i: i + sequence_size].as_matrix())
-        y.append(db.iloc[i + sequence_size].as_matrix())
+        #print(db.iloc[i : i + sequence_size])
+        #print(db.iloc[i : i + sequence_size].as_matrix().flatten())
+        x.append(db.iloc[i:i + sequence_size].as_matrix().flatten())
 
-    return np.array(x), np.array(y)
+    return np.array(x)
 
 
-def make_dataset(test_size=0.1):
+def make_dataset(test_size=0.15):
 
-    scaler = StandardScaler()
+    #scaler = StandardScaler()
     db = pd.read_csv('./wave.csv')
-    db['ok'] = scaler.fit_transform(db['ok'])
+    db['ok'] = minmax_scale(db[['ok']])
+    db['ok2'] = minmax_scale(db[['ok2']])
+  
+    #with open('parameters.txt', 'w') as f:
+    #  f.write(str(scaler.mean_[0]) + '\n')
+    #  f.write(str(scaler.var_[0]))
 
     pos = int(len(db) * (1 - test_size))
-    x_train, y_train = load_data(db['ok'].iloc[0:pos])
-    x_test, y_test = load_data(db['ok'].iloc[pos:])
-
-    return (x_train, y_train), (x_test, y_test)
+    x2_train = load_data(db['ok2'].iloc[:])
+    x_train = load_data(db['ok'].iloc[0:pos])
+    x_test = load_data(db['ok'].iloc[pos:])
+    
+    return (np.append(x_train, x2_train).reshape([-1, sequence_size]), x_test)
 
 
 def main():
 
-    (x_train, y_train), (x_test, y_test) = make_dataset()
+    (x_train, x_test) = make_dataset()
+    
+    print(x_train)
+    print(x_test)
+    #sys.exit(0)
+      
+    #model = Sequential([
+    #    LSTM(hidden, batch_input_shape=(None, sequence_size, io), return_sequences=False),
+    #    Dense(io),
+    #    Activation('linear')
+    #])
 
+    #model.compile(loss='mean_squared_error', optimizer='adam')
+  
     model = Sequential([
-        LSTM(hidden, batch_input_shape=(None, sequence_size, io), return_sequences=False),
-        Dense(io),
-        Activation('linear')
+        Dense(64, input_dim=sequence_size, activation='relu'),
+        Dense(32, activation='relu'),
+        Dense(8, activation='relu', activity_regularizer=l2(0.001)),
+        Dense(32, activation='relu'),
+        Dense(64, activation='relu'),
+        Dense(sequence_size, activation='sigmoid')
     ])
 
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    stopping = EarlyStopping(monitor='val_loss', mode='auto', patience=10)
-    model.fit(x_train, y_train, batch_size=256, epochs=100, validation_split=0.2, callbacks=[stopping])
+    model.compile(optimizer='adam', loss='mse')
 
-    result = []
-    future_steps = 400
+    stopping = EarlyStopping(monitor='val_loss', mode='auto', patience=10000)
+
+    model.fit(x_train, x_train, batch_size=128, shuffle=True, epochs=10000, validation_split=0.2, callbacks=[stopping])
+
+    #result = []
+    #future_steps = 400
     future_data = x_test[-1:][-sequence_size:]
-    for i in range(future_steps):
-        print(i)
-        pred = model.predict(future_data)
-        future_data = np.delete(future_data, 0)
-        future_data = np.append(future_data, pred[-1:])
+    #print(future_data)
+    #for i in range(future_steps):
+     #   print(i)
+     #   pred = model.predict(future_data).flatten()
+     #   future_data = np.delete(future_data, 0)
+     #   future_data = np.append(future_data, pred[-1:]).reshape(-1, sequence_size)
+#
+#        result = np.append(result, pred[-1:])
+    
+    print(len(x_train[:, -1:]))
+    tr_w = x_train[:, -1:].flatten()
+    ts_w = x_test[:, -1:].flatten()
+    input_pred = model.predict(x_test)[:,-1:].flatten()
+    print(input_pred)
 
-        result = np.append(result, pred[-1:])
-
-    input_pred = model.predict(x_test).flatten()
     plt.figure()
-    plt.plot(range(0, len(y_train)), y_train, label='train')
-    plt.plot(range(len(y_train), len(y_train) + len(y_test)), y_test, label='input')
-    plt.plot(range(len(y_train), len(y_train) + len(input_pred)), input_pred, label='input')
-    plt.plot(range(len(y_train) + len(input_pred), len(y_train) + len(input_pred) + future_steps), result, label='future')
+#    plt.plot(range(0, len(tr_w)), tr_w, label='train')
+    plt.plot(range(len(tr_w), len(tr_w) + len(ts_w)), ts_w, label='input')
+    plt.plot(range(len(tr_w), len(tr_w) + len(input_pred)), input_pred, label='predict')
+   # plt.plot(range(len(y_train) + len(input_pred), len(y_train) + len(input_pred) + future_steps), result, label='future')
     plt.title('Wave prediction')
     plt.legend(loc='upper right')
     plt.savefig('result.png')
